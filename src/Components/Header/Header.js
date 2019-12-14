794f719485dd739c6ca654535cbd11a459c87f04
emre.yilmaz@bilgeadam.com <emre.yilmaz@bilgeadam.com>
2019-12-14 21:06:59 +0300

    create project

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import './Header.scss';
+
+import {
+  Link
+} from "react-router-dom";
+
+export default class Header extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+  componentDidMount = () => {
+
+  }
+  render() {
+    return (
+      <div className="Header">
+                <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    )
+  }
+}
---
+++ src/Containers/Homepage/Homepage.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import './Homepage.scss';
+
+export default class Homepage extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+  componentDidMount = () => {
+
+  }
+  render() {
+    return (
+      <div className="Homepage">
+        Homepage
+      </div>
+    )
+  }
+}
